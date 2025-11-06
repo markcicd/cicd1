3dec2e456fe25e4b1b0e4e111063916882c80660
markcicd <mark_cicd@purplecarrot.co.uk>
2025-11-06 11:03:12 +0000

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,18 @@
+variable "gcp_project" {}
+
+provider "google" {
+  project = var.gcp_project 
+  region  = "us-west1"
+}
+
+terraform {
+  backend "gcs" {
+    bucket  = "tf-remote-state-student_04_65a21aa7a648-16993-14394"
+    prefix  = "terraform/state/lab01" 
+  }
+}
+
+resource "google_compute_network" "example_vpc" {
+  name                    = "example-vpc"
+  auto_create_subnetworks = true 
+}
